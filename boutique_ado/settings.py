8256794728b0ccb52e795999bfc5f2493ebf70ae
Ruth De Swart <ruthdeswart@outlook.ie>
2025-01-14 11:07:02 +0000

    feat: Add "Add to bag" functionality

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -73,7 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', #required by AllAuth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'bag.contexts.bag.contents',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -162,4 +162,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 FREE_DELIVERY_THRESHOLD = 50
-STANDARD_DELIVERY_PRECENTAGE = 10
+STANDARD_DELIVERY_PERCENTAGE = 10
