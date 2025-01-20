8e5223fb89ee51f97173fd900a070f58628f7e3e
Ruth De Swart <ruthdeswart@outlook.ie>
2025-01-20 10:56:35 +0000

    feat: Add forms, admin and signals

--- checkout/apps.py
+++ checkout/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class CheckoutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
+
+    def ready(self):
+        import checkout.signals
+# Overriding the ready method and importing our signals module.
+# With that done, every time a line item is saved or deleted.
+# Our custom update total model method will be called.
+# Updating the order totals automatically.
--- checkout/models.py
+++ checkout/models.py
@@ -1,4 +1,4 @@
-import uuid # Used to generate the order number
+import uuid
 
 from django.db import models
 from django.db.models import Sum
@@ -6,18 +6,18 @@ from django.conf import settings
 
 from products.models import Product
 
-# Create your models here.
+
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
-    phone_number = models.CharField(max_length=20,null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
     country = models.CharField(max_length=40, null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    county = models.CharField(max_length=40, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
@@ -26,30 +26,28 @@ class Order(models.Model):
     def _generate_order_number(self):
         """
         Generate a random, unique order number using UUID.
-        The underscore is used to indicate its a private method which will
-        only be used inside this class
+        The underscore is used to show it is a private method
+        and will only be used inside this class.
         """
-
         return uuid.uuid4().hex.upper()
 
     def update_total(self):
         """
         Update grand total each time a line item is added,
-        accounting for delivery costs
+        accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total_sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
-            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE/100
+            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
             self.delivery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
 
-
     def save(self, *args, **kwargs):
         """
         Override the original save method to set the order number
-        if it hasn't been set already
+        if it hasn't been set already.
         """
         if not self.order_number:
             self.order_number = self._generate_order_number()
@@ -59,23 +57,20 @@ class Order(models.Model):
         return self.order_number
 
 
-
-
 class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, null=False, blank=True, on_delete=models.CASCADE, related_name='lineitems')
-    product = models.ForeignKey(Product, null=False, blank=True, on_delete=models.CASCADE)
-    product_size = models.CharField(max_length=2, null=True, blank=True) #XS,S,M,L,XL
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
-    line_item_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
-
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
-            """
-            Override the original save method to set the lineitem total
-            and update the order total
-            """
-            self.line_item_total = self.product.price * self.quantity
-            super().save(*args, **kwargs)
-    
+        """
+        Override the original save method to set the lineitem total
+        and update the order total.
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return f'SKU {self.product.sku} on order {self.order.order_number}'
