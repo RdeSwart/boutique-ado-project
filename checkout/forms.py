dd8f554ebde383dc46b85727f4a66a6f4fd6d149
Ruth De Swart <ruthdeswart@outlook.ie>
2025-01-22 16:42:26 +0000

    feat: Update checkout form by installing countries

--- checkout/forms.py
+++ checkout/forms.py
@@ -20,20 +20,20 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
-            'county': 'County',
+            'county': 'County, State or Locality',
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True #setting the autofocus attribute on the full name field to true so the cursor will start in the full name field when the user loads the page.
         for field in self.fields: #iterate through the forms fields adding a star to the placeholder if it's a required field on the model.
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'#Setting all the placeholder attributes to their values in the dictionary above.
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != 'country': # Only throw error if field is not ='country'
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'#Setting all the placeholder attributes to their values in the dictionary above.
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'#Adding a CSS class
             self.fields[field].label = False #removing the form fields labels,Since we won't need them given the placeholders are now set.
